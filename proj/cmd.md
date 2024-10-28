38af7d010b8b6cffe38076f3fccbe43d89d7693a
wlgs <wielgosmikolaj@gmail.com>
2024-10-28 22:46:16 +0100

    proj milestone1 completed

---
+++ proj/cmd.md
@@ -0,0 +1,86 @@
+# Commands worth saving
+
+```bash
+arangosh --server.database wlgs --server.username root --server.password test123
+```
+
+Note: CSV must be UTF-8 coded without BOM
+
+```bash
+arangoimport --collection nodes \
+    --file "./data/nodes.csv" \
+    --type csv \
+    --server.database wlgs \
+    --server.endpoint http://localhost:8529 \
+    --server.username root \
+    --server.password test123
+```
+
+```bash
+arangoimport --collection edges \
+  --file "./data/edges.csv" \
+  --type csv \
+  --server.database wlgs \
+  --server.endpoint http://localhost:8529 \
+  --server.username root \
+  --server.password test123
+```
+
+## ArangoSH specific commands
+
+```javascript
+db._databases();
+db._useDatabase("_system");
+db._dropDatabase("wlgs");
+db._createDatabase("wlgs");
+db._useDatabase("wlgs");
+db._collections();
+db._createDocumentCollection("nodes");
+db._createEdgeCollection("edges");
+db._drop("nodes");
+db._drop("edges");
+```
+
+## data cleaning
+
+### taxonomy_iw.csv
+
+Every entry in taxonomy is of format `"\".*\",\".*\""`
+
+```bash
+> egrep "\".*\",\".*\"" ./data/taxonomy_iw.csv | wc -l
+ 5771611
+> cat ./data/taxonomy_iw.csv | wc -l
+ 5771611
+```
+
+```bash
+# check for non terminated quotes, example:
+grep -v "\"[^\"]*\"," ./data/popularity_iw.csv | wc -l 
+
+# check for non escaped quotes inside first col
+grep "\"[^\"]*\"[^\",]*\"" ./data/popularity_iw.csv | wc -l
+
+# check for comma inside quotes in first col
+grep "\"[^\"]*,[^\"]*\"" ./data/popularity_iw.csv | wc -l
+
+# check for wrongly placed commas, example:
+grep "\"[^\"]*,[^\"]*," ./data/popularity_iw.csv | wc -l
+
+# check for commas inside first col in taxonomy
+grep "^\"[^\"]*,[^\"]*\"" ./data/taxonomy_iw.csv | wc -l
+
+# check for non escaped quotes inside first col in taxonomy
+grep "^\"[^\"]*\"[^\",]*\"" ./data/taxonomy_iw.csv | wc -l
+
+egrep "(,\"){2,}" ./data/small_taxonomy_iw.csv
+
+
+# check for incorrect taxonomy entries:
+egrep -v '^\"[^\"]+\",\"[^\"]+\"' ./data/taxonomy_iw.csv | wc -l
+
+```
+
+## key information
+
+[https://docs.arangodb.com/3.11/concepts/data-structure/documents/](https://docs.arangodb.com/3.11/concepts/data-structure/documents/)
---
+++ proj/preprocess_csv.py
@@ -0,0 +1,88 @@
+import csv
+import time
+import os
+from urllib.parse import quote as urlencode
+
+PATH_POPULARITY = './data/popularity_iw.csv'
+PATH_TAXONOMY = './data/taxonomy_iw.csv'
+
+PATH_POPULARITY_TEMP = './data/popularity_iw_temp.csv'
+PATH_TAXONOMY_TEMP = './data/taxonomy_iw_temp.csv'
+
+PATH_POPULARITY_PROCESSED = './data/nodes.csv'
+PATH_TAXONOMY_PROCESSED = './data/edges.csv'
+
+SAFE_CHARACTERS = "_-.@()+=;$!*'%:"
+
+print(f"⏳ Fixing and encoding {PATH_POPULARITY}...", end='', flush=True)
+start = time.time()
+with open(PATH_POPULARITY, 'r', encoding='utf-8') as infile, open(PATH_POPULARITY_TEMP, 'w') as outfile:
+    prevRows = ["_", 0]
+    for line in infile:
+        rows = line.split(',')
+        if rows[0][-1] != '"':
+            rows[0] = f'{rows[0]}"'
+        # encode without touching the quotes
+        key = urlencode(rows[0][1:-1], safe=SAFE_CHARACTERS)
+        key = key.replace('~', '%7E')  # replace manually tilde character
+        rows[0] = f'"{key}"'
+        # do not write duplicates (may happen)
+        if (rows[0] == prevRows[0]):
+            continue
+        prevRows = rows
+        outfile.write(','.join(rows))
+print(f" ✅ {time.time() - start:.2f}s.")
+
+print(f"⏳ Fixing and encoding {PATH_TAXONOMY}...", end='', flush=True)
+start = time.time()
+with open(PATH_TAXONOMY, 'r', encoding='utf-8') as infile, open(PATH_TAXONOMY_TEMP, 'w') as outfile:
+    for line in infile:
+        rows = line.split('","')  # safest split
+        rows[1] = rows[1].rstrip('\r\n')
+
+        # cut quotes for now
+        if rows[0][0] == '"':
+            rows[0] = rows[0][1:]
+        if rows[1][-1] == '"':
+            rows[1] = rows[1][:-1]
+
+        # encode without touching the quotes
+        key_from = urlencode(rows[0], safe=SAFE_CHARACTERS)
+        key_from = key_from.replace('~', '%7E')
+        key_to = urlencode(rows[1], safe=SAFE_CHARACTERS)
+        key_to = key_to.replace('~', '%7E')
+        rows[0] = f'"{key_from}"'
+        rows[1] = f'"{key_to}"'
+        # do not write duplicates (may happen)
+        outfile.write(','.join(rows) + '\n')
+print(f" ✅ {time.time() - start:.2f}s.")
+
+print(f"⏳ Processing {PATH_POPULARITY}...", end='', flush=True)
+start = time.time()
+with open(PATH_POPULARITY_TEMP, 'r', encoding='utf-8') as infile, open(PATH_POPULARITY_PROCESSED, 'w') as outfile:
+    inputs = csv.reader(infile)
+    output = csv.writer(outfile, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_NONNUMERIC, strict=True)
+    for index, row in enumerate(inputs):
+        if index == 0:
+            output.writerow(["_key", "popularity"])
+        output.writerow(row)
+print(f" ✅ {time.time() - start:.2f}s.")
+
+
+print(f"⏳ Processing {PATH_TAXONOMY}...", end='', flush=True)
+start = time.time()
+with open(PATH_TAXONOMY, 'r', encoding='utf-8') as infile, open(PATH_TAXONOMY_PROCESSED, 'w') as outfile:
+    inputs = csv.reader(infile)
+    output = csv.writer(outfile, delimiter=',',
+                        quotechar='"', quoting=csv.QUOTE_NONNUMERIC, strict=True)
+    for index, row in enumerate(inputs):
+        if index == 0:
+            output.writerow(["_from", "_to"])
+        row[0] = f"nodes/{row[0]}"
+        row[1] = f"nodes/{row[1]}"
+        output.writerow(row)
+print(f" ✅ {time.time() - start:.2f}s.")
+
+os.remove(PATH_POPULARITY_TEMP)
+os.remove(PATH_TAXONOMY_TEMP)
