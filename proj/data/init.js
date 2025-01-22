a39d0087eaa3c7b13450671d5972334509110070
wlgs <wielgosmikolaj@gmail.com>
2025-01-22 23:21:40 +0100

    final milestone preparation

--- proj/data/init.js
+++ proj/data/init.js
@@ -1,12 +1,15 @@
-db._dropDatabase("wlgs");
-db._createDatabase("wlgs");
-db._useDatabase("wlgs");
-db._createDocumentCollection("nodes");
-db._createEdgeCollection("edges");
-var graph_module = require("@arangodb/general-graph");
-var graph = graph_module._create("graph");
-graph;
+try {
+    db._createDatabase("wlgs");
+    db._useDatabase("wlgs");
+    db._createDocumentCollection("nodes");
+    db._createEdgeCollection("edges");
+    var graph_module = require("@arangodb/general-graph");
+    var graph = graph_module._create("graph");
+    graph;
 
-graph._addVertexCollection("nodes");
-var rel = graph_module._relation("edges", ["nodes"], ["nodes"]);
-graph._extendEdgeDefinitions(rel);
+    graph._addVertexCollection("nodes");
+    var rel = graph_module._relation("edges", ["nodes"], ["nodes"]);
+    graph._extendEdgeDefinitions(rel);
+} catch (e) {
+    console.log("Database already exists");
+}
--- proj/data/init.sh
+++ proj/data/init.sh
@@ -1,19 +1,27 @@
 #!bin/sh
-arangosh --server.username root --server.password test123 --javascript.execute /data/init.js
+
+arangosh --server.username root --server.password test123 --server.endpoint http://arangodb:8529 --javascript.execute-string "console.log(db._useDatabase('wlgs'))"
+if [ $? == "0" ]; then
+  echo "[INFO]: Database already exists."
+  exit 0
+fi
+
+arangosh --server.username root --server.password test123 --server.endpoint http://arangodb:8529 --javascript.execute /data/init.js && \
 arangoimport --collection nodes \
-    --file "./data/nodes.csv" \
+    --file "/data/nodes.csv" \
     --type csv \
     --server.database wlgs \
-    --server.endpoint http://localhost:8529 \
+    --server.endpoint http://arangodb:8529 \
     --server.username root \
     --server.password test123 \
-    --on-duplicate ignore
-
+    --on-duplicate ignore \
+    --overwrite false && \
 arangoimport --collection edges \
-  --file "./data/edges.csv" \
+  --file "/data/edges.csv" \
   --type csv \
   --server.database wlgs \
-  --server.endpoint http://localhost:8529 \
+  --server.endpoint http://arangodb:8529 \
   --server.username root \
   --server.password test123 \
-  --on-duplicate ignore
+  --on-duplicate ignore \
+  --overwrite false
